ada796f1f36c1beb04708090c5fd7cc047814fb6
Piero <piero.vasquez.r@gmail.com>
2019-05-24 17:52:52 +0200

    security fail xd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,12 @@
+import { API_CONFIG } from './constants';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  configFirebase: {
-    apiKey: 'AIzaSyCB3uXEInr4BfdLVEEg4VONifsV2wlLGOo',
-    authDomain: 'portfolio-piero.firebaseapp.com',
-    databaseURL: 'https://portfolio-piero.firebaseio.com',
-    projectId: 'portfolio-piero',
-    storageBucket: 'portfolio-piero.appspot.com',
-    messagingSenderId: '120242649581',
-    appId: '1:120242649581:web:cc1bb8a21b076b2f'
-  }
+  configFirebase: API_CONFIG
 };
 
 /*
